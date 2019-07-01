e253fa1f466957fc80404e87804ad8b0afeb527d
JC Franco <jfranco@esri.com>
2019-07-01 15:09:49 -0700

    Add multiple output targets.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
 require('babel-loader');
 
-module.exports = {
-    entry: {
-        legofy: ['./src/core.js']
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: '[name].js',
-        library: 'legofy',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'stage-0']
+function createConfig(target) {
+    return {
+        entry: {
+            legofy: ['./src/core.js']
+        },
+        output: {
+            path: __dirname + '/dist',
+            filename: `[name].${target}.js`,
+            library: 'legofy',
+            libraryTarget: target
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015', 'stage-0']
+                    }
                 }
-            }
-        ]
-    }
-};
+            ]
+        }
+    };
+}
+
+module.exports = [
+    createConfig('amd'),
+    createConfig('commonjs2')
+];
